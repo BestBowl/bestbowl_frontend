7d133b9182c1b57bfc3e722a424de3e6ff188303
David Macpherson <david.allan.macpherson@gmail.com>
2023-11-16 18:39:28 -0500

    added a bit of website

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,52 +1,57 @@
 <div id="search">
-    <input placeholder="Search for questions here!">
-    <div id="params">
-        <div id="parambox">
-            <p id="paramheader">Category</p>
-            <select id="selectbox">
-                <!-- TODO -->
-                <option>PLACEHOLDER</option>
-            </select>
+    <div id="widget">
+        <input placeholder="Search for questions here!">
+        <div id="params">
+            <div id="parambox">
+                <p id="paramheader">Category</p>
+                <select id="selectbox">
+                    <!-- TODO -->
+                    <option>PLACEHOLDER</option>
+                </select>
+            </div>
+            <div id="parambox">
+                <p id="paramheader">Search Type</p>
+                <select id="selectbox">
+                    <!-- TODO -->
+                    <option>PLACEHOLDER</option>
+                </select>
+            </div>
+            <div id="parambox">
+                <p id="paramheader">Difficulty</p>
+                <select id="selectbox">
+                    <!-- TODO -->
+                    <option>PLACEHOLDER</option>
+                </select>
+            </div>
         </div>
-        <div id="parambox">
-            <p id="paramheader">Search Type</p>
-            <select id="selectbox">
-                <!-- TODO -->
-                <option>PLACEHOLDER</option>
-            </select>
+        <div id="params">
+            <div id="parambox">
+                <p id="paramheader">Subcategory</p>
+                <select id="selectbox">
+                    <!-- TODO -->
+                    <option>PLACEHOLDER</option>
+                </select>
+            </div>
+            <div id="parambox">
+                <p id="paramheader">Question Type</p>
+                <select id="selectbox">
+                    <!-- TODO -->
+                    <option>PLACEHOLDER</option>
+                </select>
+            </div>
+            <div id="parambox">
+                <p id="paramheader">Tournament</p>
+                <select id="selectbox">
+                    <!-- TODO -->
+                    <option>PLACEHOLDER</option>
+                </select>
+            </div>
         </div>
-        <div id="parambox">
-            <p id="paramheader">Difficulty</p>
-            <select id="selectbox">
-                <!-- TODO -->
-                <option>PLACEHOLDER</option>
-            </select>
-        </div>
+        
     </div>
-    <div id="params">
-        <div id="parambox">
-            <p id="paramheader">Subcategory</p>
-            <select id="selectbox">
-                <!-- TODO -->
-                <option>PLACEHOLDER</option>
-            </select>
-        </div>
-        <div id="parambox">
-            <p id="paramheader">Question Type</p>
-            <select id="selectbox">
-                <!-- TODO -->
-                <option>PLACEHOLDER</option>
-            </select>
-        </div>
-        <div id="parambox">
-            <p id="paramheader">Tournament</p>
-            <select id="selectbox">
-                <!-- TODO -->
-                <option>PLACEHOLDER</option>
-            </select>
-        </div>
+    <div id="results">
+        
     </div>
-    
 </div>
 
 <style>
@@ -55,15 +60,26 @@
         justify-content: center;
         align-items: center;
         flex-direction: column;
+        width: 100%;
+        height: 100%;
+    }
+    #widget {
+        display: flex;
+        flex-direction: column;
+
+    }
+    #results {
+        min-height: 40%;
     }
     #parambox {
-        background-color: gray;
+        border: 1px solid lightgray;
         border-radius: 0.5rem;
+        margin: 1rem;
         /* padding: 0.4rem; */
     }
     #paramheader {
-        background-color: black;
-        opacity: 0.5;
+        color: black;
+        opacity: 0.7;
         padding: 0.5rem;
         margin: 0;
 
